a1fcc95f92a65c2a8c6d06f686b31f909a7c02b5
jynnie <jynnie@mit.edu>
2017-11-17 13:36:24 -0500

    Fixed error handling in leaderboard

--- fash/controllers/leaderboard.py
+++ fash/controllers/leaderboard.py
@@ -16,34 +16,34 @@ from flask import (
 @get_user()
 def leaderboard():
     fampts = {'BFFL': 0, 'Funky Bobasaurs': 0, 'Pirates': 0, 'Greater than U': 0, 'Gangstas': 0}
-    # try:
-    engine = create_engine('sqlite:///fash.db', echo=False)
-    Session = sessionmaker(bind=engine)
-    db = Session()
+    try:
+        engine = create_engine('sqlite:///fash.db', echo=False)
+        Session = sessionmaker(bind=engine)
+        db = Session()
 
-    # CALCULATING FAMILY POINTS #
-    all_com = db.query(Completed).filter(Completed.valid==True).all()
-    for c in all_com: # Check all completed tasks
-        u = c.user # User it belongs to
-        if u != None:
-            f = db.query(Users).filter(Users.id == u).first().family # Which family this belongs to
-            fam = db.query(Families).filter(Families.id == f).first().name
-            fampts[fam] += c.task.value
-    fams = sorted(list(fampts.keys()), key=lambda x: fampts[x], reverse=True)
-    # Calculating places for families #
-    places = [1]
-    last = fampts[fams[0]]
-    for f in fams[1:]:
-        if fampts[f] == last:
-            places.append(places[-1])
-        else:
-            places.append(places[-1]+1)
-            last = fampts[f]
+        # CALCULATING FAMILY POINTS #
+        all_com = db.query(Completed).filter(Completed.valid==True).all()
+        for c in all_com: # Check all completed tasks
+            u = c.user # User it belongs to
+            if u != None:
+                f = db.query(Users).filter(Users.id == u).first().family # Which family this belongs to
+                fam = db.query(Families).filter(Families.id == f).first().name
+                fampts[fam] += c.task.value
+        fams = sorted(list(fampts.keys()), key=lambda x: fampts[x], reverse=True)
+        # Calculating places for families #
+        places = [1]
+        last = fampts[fams[0]]
+        for f in fams[1:]:
+            if fampts[f] == last:
+                places.append(places[-1])
+            else:
+                places.append(places[-1]+1)
+                last = fampts[f]
 
-    #     db.close()
-    # except Exception as e:
-    #     db.rollback()
-    #     return render_template('error.html', error='Failed to retrieve completed tasks :I', user=user)
+        db.close()
+    except Exception as e:
+        db.rollback()
+        return render_template('error.html', error='Failed to retrieve completed tasks :I', user=user)
 
     return render_template('leaderboard.html', user=user, fams=fams, fampts=fampts, places=places)
 
